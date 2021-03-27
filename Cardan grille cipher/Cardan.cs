c574b77d611e56bb562c9b91746a6d14c7e421a5
Evgeny Kopylov <zhenya.ko1711@gmail.com>
2021-03-27 23:06:42 +0700

    auto generate keycode for grille

--- Cardan grille cipher/Cardan.cs
+++ Cardan grille cipher/Cardan.cs
@@ -16,26 +16,133 @@ namespace Cardan_grille_cipher
         }
         public void digitsFillGrille(int k)
         {
+            Random rnd = new Random();
+            int[] values = new int[k * k];
+            int count = k * k;
+            int a = 0;
             for (int i = 0; i < k; i++)
                 for (int j = 0; j < k; j++)
+                {
                     FormInstanse.cardanGrille.Rows[i].Cells[j].Value = i * k + j + 1;
+                    if (count > 0)
+                    {
+                        int rand = rnd.Next(0, 2);
+                        if (rand == 1)
+                        {
+                            FormInstanse.cardanGrille.Rows[i].Cells[j].Style.BackColor = Color.Red;
+                            values[a] = Convert.ToInt32(FormInstanse.cardanGrille.Rows[i].Cells[j].Value);
+                            a++;
+                            count--;
+                        }
+                    }
+                }
 
             for (int i = k; i < k * 2; i++)
-                for (int j = k ; j < k * 2; j++)
+                for (int j = k; j < k * 2; j++)
+                {
                     FormInstanse.cardanGrille.Rows[i].Cells[j].Value = (k * 2 - i) * k + (k * 2 - j) - k;
+                    if (count > 0)
+                    {
+                        int rand = rnd.Next(0, 2);
+                        if (rand == 1)
+                        {
+                            bool flag = true;
+                            for (int n = 0; n < k * k; n++)
+                            {
+                                if (values[n] == Convert.ToInt32(FormInstanse.cardanGrille.Rows[i].Cells[j].Value)) flag = false;
+                                if (flag == false) break;
+                            }
+                            if (flag == true)
+                            {
+                                FormInstanse.cardanGrille.Rows[i].Cells[j].Style.BackColor = Color.Red;
+                                values[a] = Convert.ToInt32(FormInstanse.cardanGrille.Rows[i].Cells[j].Value);
+                                a++;
+                                count--;
+                            }
+                        }
+                    }
+                }
 
             for (int i = 0; i < k; i++)
-                for (int j = k ; j < k * 2; j++)
+                for (int j = k; j < k * 2; j++)
+                {
                     FormInstanse.cardanGrille.Rows[j].Cells[i].Value = i * k + (k * 2 - j);
+                    if (count > 0)
+                    {
+                        int rand = rnd.Next(0, 2);
+                        if (rand == 1)
+                        {
+                            bool flag = true;
+                            for (int n = 0; n < k * k; n++)
+                            {
+                                if (values[n] == Convert.ToInt32(FormInstanse.cardanGrille.Rows[i].Cells[j].Value)) flag = false;
+                                if (flag == false) break;
+                            }
+                            if (flag == true)
+                            {
+                                FormInstanse.cardanGrille.Rows[i].Cells[j].Style.BackColor = Color.Red;
+                                values[a] = Convert.ToInt32(FormInstanse.cardanGrille.Rows[i].Cells[j].Value);
+                                a++;
+                                count--;
+                            }
+                        }
+                    }
+                }
 
             for (int i = k; i < k * 2; i++)
                 for (int j = 0; j < k; j++)
+                {
                     FormInstanse.cardanGrille.Rows[j].Cells[i].Value = (k * 2 - i) * k + j - k + 1;
+                    if (count > 0)
+                    {
+                        int rand = rnd.Next(0, 2);
+                        if (rand == 1)
+                        {
+                                bool flag = true;
+                                for (int n = 0; n < k * k; n++)
+                                {
+                                    if (values[n] == Convert.ToInt32(FormInstanse.cardanGrille.Rows[i].Cells[j].Value)) flag = false;
+                                    if (flag == false) break;
+                                }
+                                if (flag == true)
+                                {
+                                    FormInstanse.cardanGrille.Rows[i].Cells[j].Style.BackColor = Color.Red;
+                                    values[a] = Convert.ToInt32(FormInstanse.cardanGrille.Rows[i].Cells[j].Value);
+                                    a++;
+                                    count--;
+                                }
+                        }
+                    }
+                }
+
+            if (count > 0)
+            {
+                for (int i = 0; i < count;)
+                {
+                    int randIdxI = rnd.Next(0, k * 2),
+                        randIdxJ = rnd.Next(0, k * 2);
+                    
+                    bool flag = true;
+                    for (int n = 0; n < k * k; n++)
+                    {
+                        if (values[n] == Convert.ToInt32(FormInstanse.cardanGrille.Rows[randIdxI].Cells[randIdxJ].Value)) flag = false;
+                        if (flag == false) break;
+                    }
+                    if (flag == true)
+                    {
+                        FormInstanse.cardanGrille.Rows[randIdxI].Cells[randIdxJ].Style.BackColor = Color.Red;
+                        values[a] = Convert.ToInt32(FormInstanse.cardanGrille.Rows[randIdxI].Cells[randIdxJ].Value);
+                        a++;
+                        count--;
+                    }
+                }    
+            }
+
         }
 
         public void generateKey(int k)
         {
-            FormInstanse.keyBox.Text = "";
+            /*FormInstanse.keyBox.Text = "";
 
             Random rnd = new Random();
             int[,] val = new int[k * 2, k * 2];
@@ -83,7 +190,7 @@ namespace Cardan_grille_cipher
                 }
             
                     
-
+            */
         }
 
         public void crypt()
